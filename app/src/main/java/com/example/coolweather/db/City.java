6967adb5f881b6ec5ba5ce2f624c1468f980a8a3
zhaohc <zhc_hope@163.com>
2022-09-13 20:53:42 +0800

    day1/数据库创建

---
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -0,0 +1,43 @@
+package com.example.coolweather.db;
+
+public class City {
+    private int id;
+
+    private String cityName;
+
+    private int cityCOde;
+
+    private int provinceID;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCOde() {
+        return cityCOde;
+    }
+
+    public void setCityCOde(int cityCOde) {
+        this.cityCOde = cityCOde;
+    }
+
+    public int getProvinceID() {
+        return provinceID;
+    }
+
+    public void setProvinceID(int provinceID) {
+        this.provinceID = provinceID;
+    }
+}
---
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -0,0 +1,43 @@
+package com.example.coolweather.db;
+
+public class County {
+    private int id;
+
+    private String countyName;
+
+    private int weatherID;
+
+    private int cityID;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getWeatherID() {
+        return weatherID;
+    }
+
+    public void setWeatherID(int weatherID) {
+        this.weatherID = weatherID;
+    }
+
+    public int getCityID() {
+        return cityID;
+    }
+
+    public void setCityID(int cityID) {
+        this.cityID = cityID;
+    }
+}
